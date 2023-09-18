202d701efdc23fc1edbe88e4cdbd82b0db23f560
AFOXJONES <alvarofabsas@gmail.com>
2023-09-18 09:37:17 +0200

    sentencias de control; P5

---
+++ src/SENTENCIAS_DE_CONTROL/PRACTICA_5/EJ3.java
@@ -0,0 +1,22 @@
+package SENTENCIAS_DE_CONTROL.PRACTICA_5;
+
+import java.util.Scanner;
+
+public class EJ3 {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+
+        //3. Leer números hasta que se introduzca un 0. Para cada uno indicar si es par o impar.
+
+
+        int num;
+        do {
+            System.out.println("Introduzca numero");
+            num = sc.nextInt();
+            if(num%2==0)
+                System.out.println("Numero par");
+            else
+                System.out.println("Numero impar");
+        } while (num != 0);
+    }
+}
