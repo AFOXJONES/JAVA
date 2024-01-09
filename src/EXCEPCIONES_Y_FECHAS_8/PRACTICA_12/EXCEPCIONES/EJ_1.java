daf6bb1b57efc88453840c992a6168f1f8befe2b
AFOXJONES <alvarofabsas@gmail.com>
2024-01-09 19:46:33 +0100

    Empezando con excepciones y fechas

---
+++ src/EXCEPCIONES_Y_FECHAS_8/PRACTICA_12/EXCEPCIONES/EJ_1.java
@@ -0,0 +1,9 @@
+package EXCEPCIONES_Y_FECHAS_8.PRACTICA_12.EXCEPCIONES;
+
+public class EJ_1 {
+    public static void main(String[] args){
+        //Programa que lee números de teclado hasta anotar un 0 y calcula su media.
+        // Usa excepciones para que si el usuario se equivoca al introducir
+        // un número se muestre un error y se vuelva a pedir.
+    }
+}
---
+++ src/EXCEPCIONES_Y_FECHAS_8/PRACTICA_12/FECHAS/EJ_1.java
@@ -0,0 +1,7 @@
+package EXCEPCIONES_Y_FECHAS_8.PRACTICA_12.FECHAS;
+
+public class EJ_1 {
+    public static void main(String[] args){
+        //Lee tres fechas de teclado (comprobando usando excepciones que son correctas) y ordénalas de menor a mayor.
+    }
+}
